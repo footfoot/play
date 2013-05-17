9d0a77bdf7d07209a752b5ead2c11f9141cc050d
Jon Maddox <jon@jonmaddox.com>
2013-05-17 12:14:52 -0700

    controls endpoints

---
+++ app/api/controls.rb
@@ -0,0 +1,19 @@
+module Play
+  class Api < Sinatra::Base
+
+    get "/now_playing" do
+    end
+
+    put "/play" do
+    end
+
+    put "/pause" do
+    end
+
+    put "/next" do
+    end
+
+    put "/previous" do
+    end
+  end
+end
