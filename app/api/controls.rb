8491dea0fbd05eaac2b4cf0ef0f3c04300d8d076
Jon Maddox <jon@jonmaddox.com>
2013-05-18 12:04:05 -0700

    dem controls

--- app/api/controls.rb
+++ app/api/controls.rb
@@ -5,12 +5,15 @@ module Play
     end
 
     put "/play" do
+      Play.client.play
     end
 
     put "/pause" do
+      Play.client.pause
     end
 
     put "/next" do
+      Play.client.next
     end
 
     put "/previous" do
